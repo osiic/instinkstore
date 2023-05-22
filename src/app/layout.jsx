1d739f40e6c3d8d59f32ef67fdefc13af9bd4d96
osi-ic <osiic.offcl@gmail.com>
2023-05-22 21:58:44 +0700

    ['import tailwindcss/tailwind.css, 'edit layout nav, main, footer', 'slicing desain navbar'

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
-import "./globals.css";
+import "tailwindcss/tailwind.css";
+import Link from "next/link";
 import { Inter } from "next/font/google";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +11,41 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={
+          (inter.className, "min-h-screen flex flex-col justify-between")
+        }
+      >
+        {/* <NavBar> */}
+        <nav className="px-[120px] py-[50px] flex justify-between items-center">
+          <h2 className="text-3xl font-semibold border-b-2 border-black">
+            Instink Store
+          </h2>
+          <div className="flex space-x-10">
+            <Link href="/" className="text-xl font-normal">
+              Home
+            </Link>
+            <Link href="/" className="text-xl font-normal">
+              About
+            </Link>
+            <Link href="/" className="text-xl font-normal">
+              Discord
+            </Link>
+            <Link href="/" className="text-xl font-normal">
+              Instagram
+            </Link>
+          </div>
+        </nav>
+        {/* <NavBar> */}
+
+        {/* <Main> */}
+        <main className="mix-w-10/12 mx-auto">{children}</main>
+        {/* </Main> */}
+
+        {/* <Footer> */}
+        <footer className="px-[120px]">Footer Boy</footer>
+        {/* </Footer> */}
+      </body>
     </html>
   );
 }
