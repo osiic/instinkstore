2f9d1d9ccd08820c8172c1fcd7961ccf09c06406
osiic <osiic.offcl@gmail.com>
2023-06-24 10:48:26 +0700

    kembali lg tanpa daisy ui

--- src/components/separate/Footer.jsx
+++ src/components/separate/Footer.jsx
@@ -1,10 +1,8 @@
 export function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-      <div>
-        <p>Copyright © 2023</p>
-        <p>All right reserved by cemy Industries Ltd</p>
-      </div>
+    <footer className="text-center text-xs font-normal py-4">
+      Copyright © 2023 <br />
+      All right reserved by cemy Industries Ltd
     </footer>
   );
 }
